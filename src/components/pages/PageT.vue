46bcbf7696e9a5fc97a94e946ab044c9423c1487
johnsonS <879917302@qq.com>
2018-11-05 16:29:14 +0800

    修改

--- src/components/pages/PageT.vue
+++ src/components/pages/PageT.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="skill">
     <title-cp title="技能"></title-cp>
-    <p class="define">let skills = [ ];</p>
-    <div class="expression">
-      <span class="express-left">skills.push(</span>
-      <div class="skill-panel" @click="curIndex = ++curIndex > 4 ? 0 : curIndex">
-        <div class="skill-item" v-show="curIndex == 0">1</div>
-        <div class="skill-item" v-show="curIndex == 1">2</div>
-        <div class="skill-item" v-show="curIndex == 2">3</div>
-        <div class="skill-item" v-show="curIndex == 3">4</div>
-        <div class="skill-item" v-show="curIndex == 4">5</div>
-      </div>
-      <span class="express-right">);</span>
-    </div>
+    
   </div>
 </template>
 
@@ -32,57 +21,13 @@ export default {
 </script>
 
 <style scoped lang="less">
-@bgColor: #646d8d;
-@fontWeight: 800;
-@skillItemWidth: 200px;
-.fontStyle () {
-  display: block;
-  font-size: 90px;
-  font-weight: @fontWeight;
-  color: #fff;
-  text-shadow: 2px 2px 30px rgba(0, 0, 0, .4);
-}
+@panelWidth: 700px;
   .skill {
-    background: @bgColor;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 40px;
     box-sizing: border-box;
-    .define {
-      margin-top: 40px;
-      font-size: 60px;
-      line-height: 60px;
-      font-weight: @fontWeight;
-      color: #fff;
-      text-shadow: 2px 2px 30px rgba(0, 0, 0, .4);
-    }
-    .expression {
-      margin-top: 60px;
-      width: 800px;
-      line-height: 180px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      .express-left {
-        .fontStyle();
-      }
-
-      .skill-panel {
-        width: @skillItemWidth;
-        height: @skillItemWidth;
-        border-radius: unit(@skillItemWidth/2, px);
-        background: #fff;
-        box-shadow: 0 0 30px rgba(0, 0, 0, .8);
-        .skill-item {
-          height: 100%;
-          text-align: center;
-        }
-      }
-
-      .express-right {
-        .fontStyle();
-      }
-    }
+    background: -webkit-linear-gradient(top, #fff3b2, #ffb6af);
   }
 </style>
--- src/components/progress/ProgressBar.vue
+++ src/components/progress/ProgressBar.vue
@@ -1,79 +1,75 @@
 <template>
   <nav class="progress">
-    <div class="nav-item">
-      <span :class="['dot',{ actived: curIndex == 0 }]">
-        <img src="https://cdn.junxio.site/static/home.svg">
+    <div class="nav-wrapper">
+      <div class="nav-item">
+        <span :class="['dot']">
       </span>
-      <div class="hint">主页</div>
-    </div>
-    <div class="nav-item">
-      <span :class="['dot',{ actived: curIndex == 1 }]">
-        <img src="https://cdn.junxio.site/static/introduce.svg">
+        <div class="hint">主页</div>
+      </div>
+      <div class="nav-item">
+        <span :class="['dot']">
       </span>
-      <div class="hint">介绍</div>
-    </div>
-    <div class="nav-item">
-      <span :class="['dot',{ actived: curIndex == 2 }]">
-        <img src="https://cdn.junxio.site/static/skill.svg">
+        <div class="hint">介绍</div>
+      </div>
+      <div class="nav-item">
+        <span :class="['dot']">
       </span>
-      <div class="hint">技能</div>
-    </div>
-    <div class="nav-item">
-      <span :class="['dot',{ actived: curIndex == 3 }]">
-        <img src="">
+        <div class="hint">技能</div>
+      </div>
+      <div class="nav-item">
+        <span :class="['dot']">
       </span>
-      <div class="hint">项目</div>
+        <div class="hint">项目</div>
+      </div>
+      <div class="nav-dot" :style="pos"></div>
     </div>
   </nav>
 </template>
-
 <script>
 import Velocity from 'velocity-animate';
 import { mapState } from 'vuex';
 export default {
   name: 'ProgressBar',
-  created () {
-  },
+  created() {},
   computed: {
-    ...mapState(['curIndex'])
+    ...mapState(['curIndex']),
+    pos () {
+      return {
+        transform: `translateY(${ this.curIndex * 30 }px)`
+      }
+    }
   }
 }
 </script>
-
 <style scoped lang="less">
 @hintActColor: rgba(0, 0, 0, .7);
-  .progress {
-    position: fixed;
-    right: 0;
-    top: 0;
-    width: 100px;
-    height: 100%;
-    transition: all .4s ease;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+
+.progress {
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100px;
+  height: 100%;
+  transition: all .4s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  .nav-wrapper {
+    position: relative;
     .nav-item {
-      width: 40px;
-      height: 40px;
+      width: 30px;
+      height: 30px;
       display: flex;
       justify-content: center;
       align-items: center;
       position: relative;
       cursor: pointer;
-      &:hover > .hint {
-        transform: translateX(-48px);
+
+      &:hover>.hint {
+        transform: translateX(-40px);
         opacity: 1;
-      }
-      &:hover .dot {
-        width: 32px;
-        height: 32px;
-        background: @hintActColor;
-        img {
-          width: 14px;
-          height: 14px;
-          opacity: 1;
-        }
       }
 
       .dot {
@@ -86,53 +82,47 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-        img {
-          width: 0px;
-          height: 0px;
-          opacity: 0;
-          transition: all .3s;
-        }
       }
 
-      .actived {
-        width: 32px;
-        height: 32px;
-        background: @hintActColor;
-        img {
-          width: 14px;
-          height: 14px;
-          opacity: 1;
-        }
-      }
 
       .hint {
         position: absolute;
         left: -14px;
-        padding: 5px 12px 5px 18px;
-        border-top-left-radius: 2px;
-        border-bottom-left-radius: 2px;
+        padding: 5px 18px;
+        border-radius: 2px;
         opacity: 0;
-        background: @hintActColor;
+        background: #fff;
         transition: all .8s;
         font-size: 12px;
         line-height: 12px;
         white-space: nowrap;
         font-weight: 600;
         text-align: center;
-        color: #fff;
-        &:after {
-          content: '';
-          display: block;
-          width: 0px;
-          height: 0px;
-          position: absolute;
-          top: 0;
-          right: -10.5px;
-          border-top: 10.5px solid transparent;
-          border-bottom: 10.5px solid transparent;
-          border-left: 10.5px solid @hintActColor;
-        }
+        color: #000;
+        box-shadow: 0 0 4px rgba(0, 0, 0, .15);
+      }
+    }
+    .nav-dot {
+      width: 30px;
+      height: 30px;
+      position: absolute;
+      left: 0;
+      top: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: transform .6s cubic-bezier(0.24,-0.98, 0.7, 1.01);
+      &:after {
+        content: '';
+        display: block;
+        width: 14px;
+        height: 14px;
+        border-radius: 7px;
+        box-shadow: 1px 1px 2px rgba(0, 0, 0, .2);
+        background: #fff;
       }
     }
   }
+  
+}
 </style>
