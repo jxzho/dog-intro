f0faf01d6df2594f7e258b8923d1c48734d15bdf
junxio <me@junxio.site>
2020-01-09 00:56:52 +0800

    chore: ico

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,30 @@
 const path = require('path');
-function resolve (dir) {
-    return path.join(__dirname, dir)
+function resolve(dir) {
+  return path.join(__dirname, dir)
 }
 module.exports = {
-    baseUrl: './',
-    lintOnSave: false,
-    chainWebpack: (config)=>{
-        config.resolve.alias
-            .set('@$', resolve('src'))
-            .set('assets',resolve('src/assets'))
-            .set('components',resolve('src/components'))
-            .set('common',resolve('src/common'))
-    },
+  baseUrl: './',
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+      .set('common', resolve('src/common'))
+    config.module
+      .rule('image')
+      .test(/\.ico$/)
+      .use('url-loader')
+      .loader('url-loader')
+  },
 
-    pluginOptions: {
-      'style-resources-loader': {
-        preProcessor: 'less',
-        patterns: [
-            path.resolve(__dirname, './src/assets/styles/theme.less'),
-            path.resolve(__dirname, './src/assets/styles/mixins.less'),
-        ]
-      }
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/theme.less'),
+        path.resolve(__dirname, './src/assets/styles/mixins.less'),
+      ]
     }
+  }
 }
