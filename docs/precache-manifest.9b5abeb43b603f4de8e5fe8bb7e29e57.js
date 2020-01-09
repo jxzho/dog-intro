ab8b0a942559937e94ffa0fbf2f0144117d56aef
钟俊雄 <37285048+junxio220@users.noreply.github.com>
2020-01-09 13:34:59 +0800

    Create Docs

---
+++ docs/precache-manifest.9b5abeb43b603f4de8e5fe8bb7e29e57.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6051b63c5ad598ceed8",
+    "url": "css/app.51dd3bbf.css"
+  },
+  {
+    "revision": "d6051b63c5ad598ceed8",
+    "url": "js/app.69637640.js"
+  },
+  {
+    "revision": "45d7c45f1f647dcadb94",
+    "url": "js/chunk-vendors.60d763d7.js"
+  },
+  {
+    "revision": "1e1dd1c5157f64ae6406ac5b22b23bdf",
+    "url": "index.html"
+  }
+];
