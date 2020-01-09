f65403e2ce12cd325930ae28381392877c8d933d
钟俊雄 <37285048+junxio220@users.noreply.github.com>
2020-01-09 10:35:39 +0800

    chore(pwa): IconPaths

--- vue.config.js
+++ vue.config.js
@@ -26,5 +26,15 @@ module.exports = {
         path.resolve(__dirname, './src/assets/styles/mixins.less'),
       ]
     }
+  },
+
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
   }
 }
