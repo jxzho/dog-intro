4b7d964e7d30bbdf47d702f9a1f022535b40cc93
钟俊雄 <37285048+junxio220@users.noreply.github.com>
2020-01-09 11:58:54 +0800

    Use Global Components

---
+++ src/plugins/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+const requireComponent = require.context(
+  '../', true, /\.vue$/
+)
+
+requireComponent.keys().slice(1).forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = capitalizeFirstLetter(
+    /([^/]+)\.vue$/.exec(fileName)[1]
+  )
+  
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
