f76ccbb4a4f29f80d1eb69026b49e6207353d685
junxio <me@junxio.site>
2020-04-12 18:11:26 +0800

    feat: Mock

---
+++ src/mock/_global.vue
@@ -0,0 +1,76 @@
+<script>
+  const cdnDomain = 'https://junxio-static.oss-cn-shenzhen.aliyuncs.com/static'
+
+  export default {
+    Home: {
+      avatarUrl: `${cdnDomain}/nur.jpg`,
+      myName: '钟俊雄',
+      greeting: 'Hi，很高兴遇到你~'
+    },
+    Introduce: {
+      pageTitle: '自我介绍',
+      brief: '热爱前端，熟悉JavaScript、Vue、React、TypeScript等',
+      birth: {
+        iconUrl: `${cdnDomain}/info/cake.svg`,
+        text: '1996.02.25'
+      },
+      university: {
+        iconUrl: `${cdnDomain}/info/university.svg`,
+        text: '东莞理工学院'
+      },
+      home: {
+        iconUrl: `${cdnDomain}/info/pin.svg`,
+        text: 'home.深圳'
+      },
+      mail: {
+        iconUrl: `${cdnDomain}/info/mail.svg`,
+        text: 'junxio220@gmail.com'
+      },
+      tel: {
+        iconUrl: `${cdnDomain}/info/phone.svg`,
+        text: '13049896626'
+      },
+      major: {
+        iconUrl: `${cdnDomain}/info/cap.svg`,
+        text: '计算机科学与技术'
+      },
+    },
+    Skill: {
+      pageTitle: '技能',
+      list: [
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/bullbasaur.svg`,
+          text: 'javaScript'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/caterpie.svg`,
+          text: 'css'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/mankey.svg`,
+          text: 'html'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/pidgey.svg`,
+          text: 'jquery'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/pikachu.svg`,
+          text: 'vue'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/psyduck.svg`,
+          text: 'node'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/snorlax.svg`,
+          text: 'ps'
+        },
+        {
+          iconUrl: `${cdnDomain}/pocket_monster/squirtle.svg`,
+          text: 'less'
+        }
+      ]
+    }
+  }
+</script>
