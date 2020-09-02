17b8c49d22033fa7a8ddd09f167bdaf86c2676a8
jxzho <879917302@qq.com>
2020-09-02 22:24:00 +0800

    style: code

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,8 +1,11 @@
 global.pages = []
+
 const pages = require.context('../pages/', false, /\.vue$/)
+
 pages.keys().forEach(key => {
   let cur = pages(key)
   const { name, label, page } = (cur.default || cur).meta
   global.pages.push({ name, label, page }) 
 })
+
 global.githubUsername = 'jxzho'
