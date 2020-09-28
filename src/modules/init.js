553694d11d499f1ce7b1c3e9d6aea1e347deda69
jx <879917302@qq.com>
2020-09-28 15:18:32 +0800

    fix: Meta

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,10 +1,9 @@
 global.pages = []
 
-const pages = require.context('../pages/', false, /\.vue$/)
-
+const pages = require.context('../pages/', true, /\.vue$/)
 pages.keys().forEach(key => {
   let cur = pages(key)
-  const { name, label, page } = (cur.default || cur).meta
+  const { name, label, page } = (cur.default || cur).meta || {}
   global.pages.push({ name, label, page }) 
 })
 
