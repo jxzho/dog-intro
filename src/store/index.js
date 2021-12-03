48d8a3bde77fa704c9f8b5c5eb66af0297517d22
Dell <dellzhong@xiaoman.cn>
2021-12-03 13:42:19 +0800

    feat: add store actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,51 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.use(Vuex);
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+const CHANGE_INDEX = 'changeIndex'
 
 const state = {
   curIndex: 0,
   transitionEnd: true,
   ifShowViewer: false,
-  url: ''
-};
+  url: '',
+}
+const actions = {
+  slideUp({ commit, state }) {
+    const val = state.curIndex - 1
+    if (val >= 0) {
+      commit(CHANGE_INDEX, val)
+      commit('changeTranEnd', false)
+    }
+  },
+  slideDown({ commit, state }) {
+    const val = state.curIndex + 1
+    if (val < 4) {
+      commit(CHANGE_INDEX, val)
+      commit('changeTranEnd', false)
+    }
+  },
+}
 
 const mutations = {
-  changeIndex (state, index) {
-    state.curIndex = index;
+  [CHANGE_INDEX](state, index) {
+    state.curIndex = index
   },
-  changeTranEnd (state, boolean) {
-    state.transitionEnd = boolean;
+  changeTranEnd(state, boolean) {
+    state.transitionEnd = boolean
   },
-  showViewer (state, url) {
-    state.ifShowViewer = true;
-    state.url = url;
+  showViewer(state, url) {
+    state.ifShowViewer = true
+    state.url = url
   },
-  hideViewer (state) {
-    state.ifShowViewer = false;
-  }
-};
+  hideViewer(state) {
+    state.ifShowViewer = false
+  },
+}
 
 export default new Vuex.Store({
   state,
-  mutations
-});
+  actions,
+  mutations,
+})
