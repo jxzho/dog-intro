aaa0c02c2c0136b61cc10373aef3e6aefbe856e5
Dell <dellzhong@xiaoman.cn>
2021-12-03 13:44:01 +0800

    feat: reg vconsole

--- src/main.js
+++ src/main.js
@@ -8,12 +8,21 @@ import { pageData } from '@/mock/index'
 
 import '@/assets/styles'
 
-Vue.config.productionTip = false
+import vConolse from 'vconsole'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+Vue.config.productionTip = isDev
 
 Vue.prototype.$eventBus = new Vue()
 Vue.prototype.$global = pageData
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
+
+if (isDev) {
+  new vConolse()
+  // console.log(window.innerWidth || document)
+}
